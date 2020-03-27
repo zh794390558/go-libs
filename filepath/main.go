8584b5799dbce0ef94f6d61a5d8f5004c5266efa
Hui Zhang <zhtclz@foxmail.com>
2020-03-27 18:14:13 +0800

    read lines

--- filepath/main.go
+++ filepath/main.go
@@ -3,9 +3,42 @@ package main
 import (
 	"fmt"
 	"os"
-
+	"bufio"
 	"path/filepath"
 )
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+  file, err := os.Open(path)
+  if err != nil {
+    return nil, err
+  }
+  defer file.Close()
+
+  var lines []string
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+  return lines, scanner.Err()
+}
+
+// writeLines writes the lines to the given file.
+func writeLines(lines []string, path string) error {
+  file, err := os.Create(path)
+  if err != nil {
+    return err
+  }
+  defer file.Close()
+
+  w := bufio.NewWriter(file)
+  for _, line := range lines {
+    fmt.Fprintln(w, line)
+  }
+
+  return nil
+}
 
 func main() {
 	fmt.Println("Base: ", filepath.Base("/tmp/main.go"))
@@ -16,4 +49,10 @@ func main() {
 	filename := "tmp.go"
 	file := filepath.Join("./uploaded/", filename)
 	fmt.Println(file)
+
+	lines, _ := readLines("./main.go")
+	for _, line := range lines{
+		fmt.Println(line)
+	}
+	return
 }
